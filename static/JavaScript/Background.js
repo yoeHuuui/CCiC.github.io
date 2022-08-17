c7f34aaed47168db38b4e50f8baed8b9a57a35a7
yoeHuuui <99573443+yoeHuuui@users.noreply.github.com>
2022-08-17 17:34:50 +0800

    Delete static directory

--- static/JavaScript/Background.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".blogtitle", {
-    strings: ['求知若饥，虚心若愚', 'Stay Hungry Stay Foolish'],
-    startDelay: 300,
-    typeSpeed: 100,
-    loop: true,
-    backSpeed: 50,
-    showCursor: true
-});
